ddf93baaff764f60cab9afe6ff1f1e5a7b6a02a4
Luiz Compan <Luizcompan2024@gmail.com>
2025-02-22 15:33:45 -0300

    Add files via upload

---
+++ templates/galeria/script.js
@@ -0,0 +1,20 @@
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+  }
+
+  document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper-container', {
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+});
+
